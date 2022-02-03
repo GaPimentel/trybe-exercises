333b2f45d40c2456ecab251895861b6557503766
Ga Pimentel <cadgap@gmail.com>
2022-02-03 20:52:48 -0300

    Cria a estrutura condicional conforme solicitado

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/arq3.js
@@ -0,0 +1,9 @@
+const notaCandidata = 60;
+
+if (notaCandidata >= 80) {
+    console.log("Parabéns, você foi aprovada(o)!");
+} else if (notaCandidata < 80 && notaCandidata >= 60) {
+    console.log("Você está na nossa lista de espera");
+} else {
+    console.log("Você foi reprovada(o)");
+}
