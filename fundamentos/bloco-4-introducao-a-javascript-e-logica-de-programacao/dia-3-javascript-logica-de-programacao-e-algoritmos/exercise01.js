480b762e165c1711365378accbbd9223e073074b
Ga Pimentel <cadgap@gmail.com>
2022-02-07 14:49:09 -0300

    Exercícios 1 ao 3

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercise01.js
@@ -0,0 +1,40 @@
+//1 Fatorial de 10:
+/* let fatorial10 = 1;
+for (let i = 2; i <= 10; i+=1) {
+  fatorial10 *= i;
+}
+console.log(fatorial10); */
+
+//2 Inverte string; 
+/* let gnirts = "você está lendo isso invertido :)";
+let invertida = [];
+let stringInvertida = "";
+for (const letra of gnirts) {
+  invertida.unshift(letra);
+}
+stringInvertida = invertida.join('');
+
+console.log(stringInvertida); */
+
+//3
+/* let array = ['java', 'javascript', 'python', 'html', 'css'];
+//Maior palavra
+let palavraMaior = "";
+for (let i = 0; i < array.length; i+=1) {
+  const element = array[i];
+  if (element.length > palavraMaior.length) {
+    palavraMaior = element;
+  }
+}
+console.log(palavraMaior);
+//Menor palavra
+let palavraMenor = "";
+for (let i = 0; i < array.length; i+=1) {
+  const element = array[i];
+  palavraMenor = array[i];
+  if (element.length < palavraMenor.length) {
+    palavraMenor = element;
+  }
+}
+console.log(palavraMenor); */
+
