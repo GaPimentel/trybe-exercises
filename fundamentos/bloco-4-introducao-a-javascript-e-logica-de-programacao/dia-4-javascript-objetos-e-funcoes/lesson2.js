c7d9799785fd0a35f8ee23f67ea8a4d50ee5a63b
Ga Pimentel <cadgap@gmail.com>
2022-02-07 21:54:04 -0300

    Lições de 1 a 2 de 2

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/lesson2.js
@@ -0,0 +1,27 @@
+//1 - "Olá xyz"
+/* let names = {
+  person1: 'João',
+  person2: 'Maria',
+  person3: 'Jorge',
+};
+function hello (object) {
+  for (const key in object) {
+    const element = object[key];
+    console.log("Olá " + element);
+  };
+}
+hello (names); */
+
+//2 - chaves e valores:
+/* let car = {
+  model: 'A3 Sedan',
+  manufacturer: 'Audi',
+  year: 2020
+};
+function carDescription (object) {
+  for (const key in object) {
+    const element = object[key];
+    console.log(key, element);
+  };
+}
+carDescription (car); */
