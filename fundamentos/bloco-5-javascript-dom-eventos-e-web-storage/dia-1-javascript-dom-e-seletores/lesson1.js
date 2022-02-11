7bfc64286275f5b0e56373a7a4a01b851603b3af
Ga Pimentel <cadgap@gmail.com>
2022-02-11 09:13:56 -0300

    Seleção de elementos e nós

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/lesson1.js
@@ -0,0 +1,12 @@
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+//título da página: alterá-lo para o nome do seu filme favorito.
+const title1 = document.getElementById("page-title");
+title1.innerText = "E.T. O Extraterrestre";
+//segundo parágrafo: alterá-lo.
+const secondParagraph = document.getElementById("second-paragraph");
+secondParagraph.innerText = "Com um roteiro que emocionou gerações, o filme de Spielberg apresenta a possibilidade de vida extraterrestre através de uma perspectiva bem diferente do comum.Elliott é um garoto que descobre um ser alienígena que caiu na Terra e inicia uma amizade com ele. Com o apoio dos irmãos, ele decide escondê-lo do governo e ajudá-lo a regressar para o seu planeta de origem.";
+
+//subtítulo: altere-o.
+const subTitle = document.getElementById("subtitle");
+subTitle.innerText = "Texto retirado de : https://www.culturagenial.com/melhores-filmes-de-ficcao-cientifica/"
