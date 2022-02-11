d9f7b1f5917428edf9791ba1834d2fdac41d6962
Ga Pimentel <cadgap@gmail.com>
2022-02-11 13:43:06 -0300

    Cria os arquivos para os exercicios do dia 5.2

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/lesson1.js
@@ -0,0 +1,24 @@
+/* // arquivo script.js
+
+console.log(document.getElementById('start').nextSibling) // nó
+
+console.log(document.getElementById('start').nextElementSibling) // <p>elemento</p>
+ */
+
+//1 elementoOndeVoceEsta
+
+//2 add cor no pai de elementoOndeVoceEsta
+
+//3 primeiroFilhoDoFilho - adicionar texto
+
+//4 primeiroFilho a partir de pai
+
+//5 primeiroFilho a partir de elementoOndeVoceEsta
+
+//6 acesse o texto Atenção! a partir de elementoOndeVoceEsta
+
+//7 terceiroFilho a partir de elementoOndeVoceEsta
+
+//8 terceiroFilho a partir de pai
+
+//9 Remover todos os elementos filhos de paiDoPai exceto pai , elementoOndeVoceEsta e primeiroFilhoDoFilho
